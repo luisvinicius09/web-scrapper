7efe4c2b502db77408e48626d41ca6be8998226e
Luis V <luisvinicius0906@gmail.com>
2023-11-12 19:56:39 -0300

    Add: logs folder and few todo

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import axios from 'axios';
 import { tasksUrl } from './constants.js';
 import { writeFile } from 'fs';
 
+// TODO: App will run constantly 
+// TODO: There will be keypress events to trigger stuff on console, like clear & run
+
 axios
 	.get(tasksUrl)
 	.then((res) => {
 		console.log(res.data);
 
-		writeFile('data.html', res.data, (err) => {
+		writeFile('logs/data.html', res.data, (err) => {
 			if (err) throw err;
 
 			console.log('The file has been saved!');
